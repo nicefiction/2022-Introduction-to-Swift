83f6547394a56b4278cdbe0f7ac9643d7a492504
Olivier Van hamme <nicefiction@gmail.com>
2022-02-18 17:34:16 +0100

    Completes Section 3 of Day 10: How to take action when a property changes.

--- Introduction to Swift.playground/Pages/Day 10 • Structs.xcplaygroundpage/Contents.swift
+++ Introduction to Swift.playground/Pages/Day 10 • Structs.xcplaygroundpage/Contents.swift
@@ -88,7 +88,8 @@ struct Employee1 {
     var vacationRemaining: Int
 }
 
-var employee1 = Employee1(name: "Dorothy", vacationRemaining: 14)
+var employee1 = Employee1(name: "Dorothy",
+                          vacationRemaining: 14)
 
 employee1.vacationRemaining -= 5
 print(employee1.vacationRemaining)
@@ -157,5 +158,147 @@ employee3.vacationRemaining = 3
 ///`Lazy properties` help mitigate the performance issues of _rarely read stored properties_,
 ///and `property observers` mitigate the dependency problems of stored properties
 ///— we’ll be looking at them soon.
+
+
+
+/// How to take action when a property changes
+/// https://www.hackingwithswift.com/quick-start/beginners/how-to-take-action-when-a-property-changes
+/// TIP:
+/// Please try to avoid putting too much work into property observers
+/// — if something that looks trivial such as `game.score += 1` triggers intensive work,
+/// it will catch you out on a regular basis and cause all sorts of performance problems.
+/// NOTE
+/// SwiftUI uses `willSet` in some places to handle animations
+/// so that it can take a snapshot of the user interface before a change.
+/// When it has both the “before” and “after” snapshot,
+/// it can compare the two to see all the parts of the user interface that need to be updated.
+
+struct Game {
+    
+    var score: Int
+}
+
+var game = Game.init(score: 20)
+game.score -= 5
+print("The score is now \(game.score).")
+
+
+struct Play {
+    var score: Int {
+        /// `willSet`runs  code _before_ the property changes:
+        willSet {
+            print("The play score will be \(newValue).")
+        }
+        /// `didSet`runs  code when the property has changed:
+        didSet {
+            print("The play score was \(oldValue).")
+            print("The play score is now \(score).")
+        }
+    }
+}
+
+var play = Play.init(score: 30)
+play.score -= 5
+
+
+
+struct App {
+    
+    var title: String = "anaonymous" {
+        willSet {
+            print("The current value is: \(title)")
+            print("The new value will be: \(newValue)")
+        }
+        
+        didSet {
+            print("The new value is: \(title)")
+            print("The old value was: \(oldValue)")
+        }
+    }
+    
+    var contacts = Array<String>() {
+        willSet {
+            print("The current array is: \(contacts)")
+            print("The new array will be: \(newValue)")
+        }
+        
+        didSet {
+            print("The new array is: \(contacts)")
+            print("The old array was: \(oldValue)")
+        }
+    }
+}
+
+
+var app = App.init(title: "Hello World")
+app.title = "Hello Again"
+app.contacts.append("Dorothy")
+app.contacts.append("Ozma")
+
+
+
+struct BankAccount {
+    
+    var name: String
+    var isMillionaire: Bool = false
+    var balance: Int {
+        didSet {
+            if balance > 1_000_000 {
+                isMillionaire = true
+            } else  {
+                isMillionaire = false
+            }
+        }
+    }
+}
+
+
+struct Child {
+    
+    var name: String
+    var age: Int {
+        
+        didSet {
+            print("Happy birthday \(name).")
+        }
+    }
+}
+
+var child = Child.init(name: "Dorothy",
+                       age: 11)
+child.age = 15
+
+
+struct Dog {
+    
+    var age: Int {
+        didSet {
+            let dogAgeToHumanAge = age * 7
+            print("The dog is now \(dogAgeToHumanAge) years old in Human years.")
+        }
+    }
+}
+
+var dog = Dog.init(age: 3)
+dog.age = 5
+
+
+struct Student {
+    
+    var debt: Int {
+        didSet {
+            switch debt {
+            case 0..<1_000: print("Okay debt")
+            case 1_000..<5_000: print("Reasonable debt")
+            case 5_000..<10_000: print("Sizeable debt")
+            default: print("Can I fake my own death?")
+            }
+        }
+    }
+}
+
+var student = Student.init(debt: 100)
+student.debt = 3_000
+
 
 //: [Next](@next)
